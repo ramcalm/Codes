ffbd9d4c5058514bca8000cef10cf8d56d1e592c
Ram Gunasekaran A <ram.callm@gmail.com>
2021-12-25 22:57:20 +0530

    Time: 4 ms (99.56%), Space: 10.7 MB (52.04%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.find(target-nums[i])!=mp.end())
+                return {mp[target-nums[i]],i};
+            mp[nums[i]]=i;
+        }
+        return {};
+    }
+};
+
+auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
