f568c21e0468e087990db1d20202dbeaef3fe9a1
Ram Gunasekaran A <ram.callm@gmail.com>
2022-01-15 00:20:57 +0530

    Time: 6 ms (20.93%), Space: 7 MB (52.53%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int myAtoi(string str) {
+        int i = 0;
+        int sign = 1;
+        int result = 0;
+        if (str.length() == 0) return 0;
+
+        while (i < str.length() && str[i] == ' ')
+            i++;
+
+        if (i < str.length() && (str[i] == '+' || str[i] == '-'))
+            sign = (str[i++] == '-') ? -1 : 1;
+
+        while (i < str.length() && str[i] >= '0' && str[i] <= '9') {
+            if (result > INT_MAX / 10 ||
+                    (result == INT_MAX / 10 && str[i] - '0' > INT_MAX % 10)) {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+            result = result * 10 + (str[i++] - '0');
+        }
+        return result * sign;
+    }
+};
