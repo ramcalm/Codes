51ee5654fd2c4020a83d5467233096993223d059
Ram Gunasekaran A <ram.callm@gmail.com>
2022-02-15 21:12:29 +0530

    Time: 443 ms (24.89%), Space: 17.6 MB (11.43%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+   int dp[10001];
+    int solve(vector<int>&nums,int i,int n){
+        if(i==n){
+            return dp[i] = 0;
+        }
+        if(i>n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int c = INT_MAX-2;
+        for(int j=1;j<=nums[i];j++){
+            c = min(c,1+solve(nums,i+j,n));
+        }
+        return dp[i] = c;
+    }
+    int jump(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        return solve(nums,0,nums.size()-1);
+    }
+};
