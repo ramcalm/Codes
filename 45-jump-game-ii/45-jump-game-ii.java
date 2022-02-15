f5fbd2953602040b0d62a8ba1ae3334ec9e96615
Ram Gunasekaran A <ram.callm@gmail.com>
2022-02-15 21:01:33 +0530

    Time: 236 ms (11.45%), Space: 51 MB (5.65%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+        return helper(nums, 0, new Integer[nums.length]);
+    }
+    
+    public int helper(int[] nums, int index, Integer[] dp){
+        
+        if(index>=nums.length-1)
+            return 0;
+        
+        if(dp[index]!=null)
+            return dp[index];
+        
+        int min= Integer.MAX_VALUE/2;
+        for(int i=1;i<=nums[index];i++){
+            min=Math.min(min, 1+helper(nums, index+i, dp));
+        }
+        
+        dp[index]=min;
+        return min;
+    }
+}
