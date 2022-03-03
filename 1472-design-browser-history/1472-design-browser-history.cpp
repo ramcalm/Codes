87af2f29029d346f2f9a98dc1affbd12fe86208b
Ram Gunasekaran A <ram.callm@gmail.com>
2022-03-03 11:09:22 +0530

    Time: 184 ms (63.25%), Space: 60.8 MB (36.37%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -0,0 +1,56 @@
+class Node
+{
+    public:
+    string data;
+    Node *prev,*next;
+    
+    Node()
+    {
+        data="";
+        prev=next=NULL;
+    }
+    Node(string _data)
+    {
+        data=_data;
+        prev=next=NULL;
+    }
+};
+class BrowserHistory {
+public:
+    
+    Node *curr=new Node();
+    BrowserHistory(string homepage) {
+        Node *begin=new Node(homepage);
+        begin->prev=curr;
+        curr->next=begin;
+        curr->prev=NULL;
+        curr=curr->next;
+        
+    }
+    
+    void visit(string url) {
+        Node *newNode=new Node(url);
+        curr->next=newNode;
+        newNode->prev=curr;
+        newNode->next=NULL;
+        curr=curr->next;
+    }
+    
+    string back(int steps) {
+        
+        while(steps-- and curr->prev->data!="")
+        {
+            curr=curr->prev;
+		}
+        return curr->data;
+    }
+    
+    string forward(int steps) {
+        
+        while(steps-- and curr->next)
+        {
+            curr=curr->next;
+        }
+        return curr->data;
+    }
+};
