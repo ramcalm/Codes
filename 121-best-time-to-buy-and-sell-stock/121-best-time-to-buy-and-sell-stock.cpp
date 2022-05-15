e32d4bc365d87b0738fdc4d64d55db5e3c5f9f3f
Ram Gunasekaran A <ram.callm@gmail.com>
2022-05-16 01:50:34 +0530

    Time: 186 ms (34.81%), Space: 93.3 MB (53.84%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -5,10 +5,8 @@ public:
         int minprice=INT_MAX;
         for(int i=0;i<prices.size();i++)
         {
-            if(prices[i]<minprice)
-                minprice=prices[i];
-            else if(prices[i]-minprice > profit)
-                profit=prices[i]-minprice;
+            minprice=min(minprice,prices[i]);
+            profit=max(profit,prices[i]-minprice);
         }
         return profit;
     }
